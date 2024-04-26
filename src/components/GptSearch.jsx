e56a14e0b3e036c8d6ad54630d13d3fddf2ceb69
Amulya2823 <amulyareddy58@gmail.com>
2024-04-26 18:02:56 +0530

    Made the GPTSearch Page

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import GptSearchBar from './GptSearchBar';
-import GptMovieSuggestions from './GptMovieSuggestions';
+import React from "react";
+import GptSearchBar from "./GptSearchBar";
+import GptMovieSuggestions from "./GptMovieSuggestions";
+import { BACK_IMG } from "../utils/constants";
 
 const GptSearch = () => {
   return (
     <div>
-      <GptSearchBar/>
-      <GptMovieSuggestions/>
+      <div className="absolute -z-20">
+        <img src={BACK_IMG} alt="logo" />
+      </div>
+
+      <GptSearchBar />
+      <GptMovieSuggestions />
     </div>
-  )
-}
+  );
+};
 
 export default GptSearch;
--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const GptSearchBar = () => {
   return (
-    <div>
-      <form>
-        <input type='text'></input>
+    <div className='pt-[10%] flex justify-center '>
+      <form className='p-2 rounded-lg w-1/2 bg-black text-white grid grid-cols-12'>
+        <input className=' p-2 col-span-9 text-lg text-black ' type='text' placeholder='What do you wanna watch?'></input>
+        <button className='p-4 col-span-3 bg-red-500 text-xl font-semibold rounded-lg'>Search</button>
       </form>
     </div>
   )
